2cc66af1e9333fb58e54860981e18ad8144c8c45
liujiarui <liujiarui@ship56.net>
2018-03-18 00:42:48 +0800

    添加要完成的任务清单

--- app/src/main/java/cn/leo/localnetframe/bean/State.kt
+++ app/src/main/java/cn/leo/localnetframe/bean/State.kt
@@ -1,8 +1,6 @@
 package cn.leo.localnetframe.bean
 
 import android.support.annotation.IntDef
-import java.lang.annotation.Retention
-import java.lang.annotation.RetentionPolicy
 
 /**
  * Created by Leo on 2018/3/14.
@@ -11,5 +9,5 @@ const val STATE_ONLINE = 1L
 const val STATE_OFFLINE = 2L
 
 @IntDef(STATE_ONLINE, STATE_OFFLINE)
-@Retention(RetentionPolicy.SOURCE)
+@Retention(AnnotationRetention.SOURCE)
 annotation class State
